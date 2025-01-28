288d861146c5cbe8a66d64f204d4930a42acc71f
yahirRamirezVal <yahir.ramirez@grupobecm.com>
2025-01-28 16:23:30 -0600

    arreglo de videos

--- src/components/Intro/VideoSlider.tsx
+++ src/components/Intro/VideoSlider.tsx
@@ -1,23 +1,29 @@
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactPlayer from "react-player";
 import Slider from "react-slick";
+import {
+  PauseCircleFilled,
+  PlayCircleFilled
+} from '@ant-design/icons';
 import VideoIntro from "../../assets/video/video_intro.mp4";
-
 
 const VideoSlider = () => {
   const [currentIndex, setCurrentIndex] = useState<number | null>(null);
-  const [isPlayingVideo, setIsPlayingVideo] = useState<boolean>(false);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isMuted, setIsMuted] = useState<boolean>(true);
   const holderRef = useRef<HTMLDivElement>(null);
+  const videoRefs = useRef<ReactPlayer[]>([]);
 
   const handleScroll = () => {
     const element = holderRef.current;
     if (element) {
       const elementTop = element.offsetTop;
       const scrollTop = window.scrollY;
-      if (scrollTop >= elementTop) {
-        setIsPlayingVideo(true);
+      if (scrollTop > 0 && scrollTop >= elementTop) {
+        setCurrentIndex(0);
       } else {
-        setIsPlayingVideo(false);
+        setCurrentIndex(null);
+        setIsPlaying(false);
       }
     }
   };
@@ -31,8 +37,16 @@ const VideoSlider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: false,
-    afterChange: (currentSlide:number) => {
-      setCurrentIndex(currentSlide);
+    afterChange: (index: number) => {
+      setCurrentIndex(index);
+      videoRefs.current.forEach((player, i) => {
+        if (i === index) {
+          player.getDuration();
+          setIsPlaying(true);
+        } else {
+          setIsMuted(true);
+        }
+      });
     },
   };
 
@@ -40,28 +54,52 @@ const VideoSlider = () => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      setIsPlayingVideo(false);
+      setCurrentIndex(null);
     };
   }, []);
-  
+
   return (
     <div className="video-slider" ref={holderRef}>
       <Slider {...videosSettings}>
-        {videos.map((video, index)=>(
-          <div>
-          <div key={index} className="video-holder">
-            <ReactPlayer
-              playing={isPlayingVideo && (currentIndex === index)}
-              url={video}
-              width="100%"
-              height="85%"
-            />
+        {videos.map((video, index) => (
+          <div key={`holder-${index}`}>
+            <div key={index} className="video-holder">
+              <ReactPlayer
+                onPlay={() => {
+                  if (index === currentIndex) {
+                    setIsMuted(false);
+                  } else {
+                    setIsMuted(true);
+                  }
+                }}
+                ref={(el) => (videoRefs.current[index] = el!)}
+                playing={isPlaying === true && index === currentIndex}
+                url={video}
+                width="100%"
+                height="87%"
+                muted={isMuted}
+              />
+
+              <footer className="video-slider__bottoms">
+                {isPlaying && (
+                  <button onClick={() => setIsPlaying(false)}>
+                    <PauseCircleFilled />
+                  </button>
+                )}
+
+                {!isPlaying && (
+                  <button onClick={() => setIsPlaying(true)}>
+                    {" "}
+                    <PlayCircleFilled />
+                  </button>
+                )}
+              </footer>
+            </div>
           </div>
-        </div>
         ))}
       </Slider>
     </div>
   );
-}
- 
+};
+
 export default VideoSlider;
