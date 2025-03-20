1523fa2e9d50afe1cb62d08f97014e638be86f7b
yahirRamirezVal <yahir.ramirez@grupobecm.com>
2025-03-19 18:05:07 -0600

    cambios en archivos de configuracion

--- src/components/Intro/VideoSlider.tsx
+++ src/components/Intro/VideoSlider.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 import Slider from "react-slick";
 import { PauseCircleFilled, PlayCircleFilled } from "@ant-design/icons";
